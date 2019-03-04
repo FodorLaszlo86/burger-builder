ab0795f769a969d83b409e92243c501bf039270f
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-03-04 23:50:35 +0100

    rendering a static burger (for now)

--- src/components/Burger/BurgerIngredients/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredient.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import classes from './burgerIngredient.css';
+import classes from './BurgerIngredient.css';
 import PropTypes from 'prop-types';
 
 class BurgerIngredient extends Component {
     render() {
         let ingredient = null;
 
-        switch(props.type) {
+        switch(this.props.type) {
             case ('bread-bottom'):
                 ingredient = <div className={ classes.BreadBottom } ></div>;
                 break;
@@ -38,8 +38,8 @@ class BurgerIngredient extends Component {
     
 }
 
-burgerIngredient.propTypes = {
+BurgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
 }
 
-export default burgerIngredient;
+export default BurgerIngredient;
