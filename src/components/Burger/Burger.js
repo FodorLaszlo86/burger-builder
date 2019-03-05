aca586dc1cc1f671b2e6b46d966ce9266e5776f5
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-03-05 10:52:13 +0100

    build burger, array of ingredients and reference of func adding ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
 
-const burger = props => {
+const burger = ({ ingredients }) => {
+  let transformedIngredients = Object.keys(ingredients)
+        .map(igKey => {
+          return [...Array(ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={ igKey + i } type={ igKey } />
+          })
+        }).reduce((arr, el) => { return arr.concat(el) }, []);
+
+        if(transformedIngredients.length === 0) {
+          transformedIngredients = <p>Please Start Adding Ingredients!</p>
+        }
     return (
       <div className={ classes.Burger }>
         <BurgerIngredient type='bread-top'/>
-        <BurgerIngredient type='cheese'/>
-        <BurgerIngredient type='meat'/>
+        { transformedIngredients }
         <BurgerIngredient type='bread-bottom'/>
       </div>
     )
